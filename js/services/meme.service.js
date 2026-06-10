0b20652f4f98cb6729eb10a2834caab35add2fd7
noyMoyal <noymoyyal@gmail.com>
2026-06-10 22:59:44 +0300

    Fix selected line color and size

--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -1,64 +1,59 @@
-'use strict'
+"use strict"
 
 var gMeme = {
-    selectedImgId: 1,
-    selectedLineIdx: 0,
-    lines: [
-        {
-            txt: 'Hello, Meme!',
-            size: 40,
-            color: 'white',
-        }
-        
-    ]
+  selectedImgId: 1,
+  selectedLineIdx: 0,
+  lines: [
+    {
+      txt: "Hello, Meme!",
+      size: 40,
+      color: "white",
+    },
+  ],
 }
-                   
+
 function getMeme() {
-    return gMeme
+  return gMeme
 }
 
-
-//which line to edit? the first or the second?(no hc)
+//Update the selected line text
 function setLineTxt(txt) {
-    gMeme.lines[gMeme.selectedLineIdx].txt = txt
+  gMeme.lines[gMeme.selectedLineIdx].txt = txt
 }
-
 
 function setImg(imgId) {
-    gMeme.selectedImgId = imgId
+  gMeme.selectedImgId = imgId
 }
 
 function setColor(color) {
-    gMeme.lines[0].color = color
+  gMeme.lines[gMeme.selectedLineIdx].color = color
 }
 
-
 function increaseFontSize() {
-    gMeme.lines[0].size += 2
-    }
-
+  gMeme.lines[gMeme.selectedLineIdx].size += 2
+}
 
 function decreaseFontSize() {
-    gMeme.lines[0].size -= 2
-    }
-    function addLine() {
-        if (gMeme.lines.length >= 2) return
+  gMeme.lines[gMeme.selectedLineIdx].size -= 2
+}
+function addLine() {
+  if (gMeme.lines.length >= 2) return
 
-        // new line with push (array method)
-        gMeme.lines.push({
-            txt: 'New Line',
-            size: 40,
-            color: 'white',
-        })
-    }
-
+  // Add a new line using push
+  gMeme.lines.push({
+    txt: "New Line",
+    size: 40,
+    color: "white",
+  })
+  // after adding a new line, we want to edit it, so we switch to it
+  gMeme.selectedLineIdx = 1
+}
 
 //toggle first line/second line (if exists)
-    function switchLine() {
-        //Switch line only works after a second line was added
-        if(gMeme.lines.length < 2) return
 
-        gMeme.selectedLineIdx = gMeme.selectedLineIdx === 0 ? 1 : 0
+function switchLine() {
+  //Switch line only works after a second line was added
+  if (gMeme.lines.length < 2) return
 
-        console.log(gMeme.selectedLineIdx)
-    }
+  gMeme.selectedLineIdx = gMeme.selectedLineIdx === 0 ? 1 : 0
+}
